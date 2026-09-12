f3396b17c58b915be952234dacca23635d5edc2a
Paras Patel <38793371+paras160500@users.noreply.github.com>
2026-09-12 09:58:24 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -24,7 +24,8 @@ class Settings(BaseSettings):
     chinook_sql_url : str = "https://raw.githubusercontent.com/lerocha/chinook-database/master/ChinookDatabase/DataSources/Chinook_Sqlite.sql"
 
     # API/CORS
-    frontend_origins : str = "http://localhost:5173,http://localhost:3000,https://wavelength-multi-agent-orchestration.onrender.com/"
+    frontend_origins: str = "http://localhost:5173,http://localhost:3000,https://wavelength-multi-agent-orchestration.onrender.com"
+
     model_config = SettingsConfigDict(env_file=".env" , extra="ignore")
 
     @property
